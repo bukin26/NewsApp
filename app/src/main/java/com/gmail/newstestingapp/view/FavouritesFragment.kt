2a733bc899750e970b7da479b6d82390c18e0c7b
bukin26 <mg.bukin26@gmail.com>
2022-03-18 23:15:06 +0200

    added adapter its initialization to video and favourites fragment

--- app/src/main/java/com/gmail/newstestingapp/view/FavouritesFragment.kt
+++ app/src/main/java/com/gmail/newstestingapp/view/FavouritesFragment.kt
@@ -5,8 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gmail.newstestingapp.R
+import com.gmail.newstestingapp.databinding.FragmentFavouritesBinding
+import com.gmail.newstestingapp.databinding.FragmentStoriesBinding
+import com.gmail.newstestingapp.viewmodel.FavouritesViewModel
+import com.gmail.newstestingapp.viewmodel.StoriesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class FavouritesFragment : Fragment(R.layout.fragment_favourites)
+class FavouritesFragment : Fragment(R.layout.fragment_favourites) {
+    private lateinit var binding: FragmentFavouritesBinding
+    private val viewModel: FavouritesViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentFavouritesBinding.inflate(inflater, container, false)
+
+        setUpGifsList()
+
+        return binding.root
+    }
+
+    private fun setUpGifsList() {
+        val adapter = NewsAdapter()
+        binding.favouritesRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.favouritesRecyclerView.adapter = adapter
+        (binding.favouritesRecyclerView.itemAnimator as? DefaultItemAnimator)?.supportsChangeAnimations =
+            false
+
+        observeUsers(adapter)
+    }
+
+    private fun observeUsers(adapter: NewsAdapter) {
+        lifecycleScope.launch {
+            viewModel.newsFlow.collectLatest { pagingData ->
+                adapter.submitData(pagingData)
+            }
+        }
+    }
+}
--- app/src/main/java/com/gmail/newstestingapp/view/VideoFragment.kt
+++ app/src/main/java/com/gmail/newstestingapp/view/VideoFragment.kt
@@ -5,8 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gmail.newstestingapp.R
+import com.gmail.newstestingapp.databinding.FragmentFavouritesBinding
+import com.gmail.newstestingapp.databinding.FragmentStoriesBinding
+import com.gmail.newstestingapp.databinding.FragmentVideoBinding
+import com.gmail.newstestingapp.viewmodel.FavouritesViewModel
+import com.gmail.newstestingapp.viewmodel.StoriesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class VideoFragment : Fragment(R.layout.fragment_video)
+@ExperimentalCoroutinesApi
+class VideoFragment : Fragment(R.layout.fragment_video) {
+    private lateinit var binding: FragmentVideoBinding
+    private val viewModel: StoriesViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentVideoBinding.inflate(inflater, container, false)
+
+        setUpGifsList()
+
+        return binding.root
+    }
+
+    private fun setUpGifsList() {
+        val adapter = NewsAdapter()
+        binding.videoRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.videoRecyclerView.adapter = adapter
+        (binding.videoRecyclerView.itemAnimator as? DefaultItemAnimator)?.supportsChangeAnimations =
+            false
+
+        observeUsers(adapter)
+    }
+
+    private fun observeUsers(adapter: NewsAdapter) {
+        lifecycleScope.launch {
+            viewModel.newsFlow.collectLatest { pagingData ->
+                adapter.submitData(pagingData)
+            }
+        }
+    }
+}
