fa7514f2b53981ef3d1821108f78e82dd7ec75b4
bukin26 <mg.bukin26@gmail.com>
2022-03-18 16:02:53 +0200

    added fragments and nva_graph

---
+++ app/src/main/java/com/gmail/newstestingapp/view/StoriesFragment.kt
@@ -0,0 +1,10 @@
+package com.gmail.newstestingapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gmail.newstestingapp.R
+
+class StoriesFragment : Fragment(R.layout.fragment_stories)
